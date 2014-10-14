1edd1800b246ea59251a97351d6c9c9d01a4c214
Vic W <vwalldev@gmail.com>
2014-10-14 02:20:43 +0000

    Finished Scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+x=1
+10.times do 
+  Product.create!(
+    title: "Awesome, version #{x.to_s}",
+    description: 
+      "<p>Awesome is the most excellent book series to be created, ever in the universe.  Version #{x.to_s} is the         most prolific version to ever be created.</p>",
+    image_url: 'ruby.jpg',
+    price: 49.93
+  )
+  x+=1
+end
